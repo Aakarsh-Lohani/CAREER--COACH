fce735a83da7402eed5833684956de7371deb74d
akshatkumarsingh047 <akshatkumarsingh047@gmail.com>
2025-06-13 21:40:00 +0530

    line chart line color change

--- app/(main)/interview/_components/performace-chart.jsx
+++ app/(main)/interview/_components/performace-chart.jsx
@@ -67,7 +67,7 @@ export default function PerformanceChart({ assessments }) {
               <Line
                 type="monotone"
                 dataKey="score"
-                stroke="hsl(var(--primary))"
+                stroke="#0000ff"
                 strokeWidth={2}
               />
             </LineChart>
