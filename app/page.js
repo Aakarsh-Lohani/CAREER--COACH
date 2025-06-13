40c95028adb4c72b48cbe0f8998d46f55030d9c3
akshatkumarsingh047 <akshatkumarsingh047@gmail.com>
2025-06-13 15:38:57 +0530

    error resolved

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 export default async function LandingPage() {
   const { userId } = await auth();
-    if (!userId) throw new Error("Unauthorized");
+   
   
     const user = await db.user.findUnique({
       where: { clerkUserId: userId },
